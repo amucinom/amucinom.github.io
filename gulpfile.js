55a9f89760998fbfc7c03ae43a828b745b6b1e92
amucinom <amucinom@gmail.com>
2016-05-08 22:23:19 -0700

    Changed gulp task to browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,26 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
 var serve = require('gulp-server-livereload');
 
-gulp.task('serve', function() {
-    gulp.src('.')
-    	.pipe(serve({
-    		livereload: true,
-    		directoryListing: false,
-    		open: true,
-    	}));
+// gulp.task('serve', function() {
+//     gulp.src('.')
+//     	.pipe(serve({
+//     		livereload: true,
+//     		directoryListing: false,
+//     		open: true,
+//     	}));
+// });
+
+gulp.task('browser-sync', function () {
+
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    gulp.watch("styles/*.css").on("change", browserSync.reload);
+    gulp.watch("*.html").on("change", browserSync.reload);
+    gulp.watch("pages/*.html").on("change", browserSync.reload);
+    gulp.watch("scripts/*.js").on("change", browserSync.reload);
 });
