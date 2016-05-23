3cf0bb98f33ba2c21f5df4dfe54ee0bb15688fed
Aldo Mucino <amucinom@gmail.com>
2016-05-23 12:00:48 -0700

    Fixed height of card images

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 
 		var top = $(document).scrollTop() || $('body').scrollTop() || $('html').scrollTop();
 		if (top > 0) {
-			$('.nav').css('box-shadow', '0 2px 5px 0 rgba(0, 0, 0, 0.26');
+			$('.nav').css('box-shadow', '0 0 4px rgba(0,0,0,.14), 0 4px 8px rgba(0,0,0,.28)');
 		} else if (top === 0) {
 			$('.nav').css('box-shadow', 'none');
 		}
