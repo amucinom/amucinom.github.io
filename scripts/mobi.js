e7c93f74096af3137c5a3b2bcb23d0cc53ad5a40
Aldo Mucino <amucinom@gmail.com>
2016-05-30 13:41:41 -0700

    Fixed page-card layout

---
+++ scripts/mobi.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+	if (/Mobi/.test(navigator.userAgent)) {
+    	// $('#masthead').removeClass('wrapper');
+    	// $('#hero').removeClass('wrapper');
+    	// $('#footer').removeClass('wrapper');
+    	// $('#card-container').removeClass('wrapper');
+    	$('#remove').removeClass('wrapper');
+	}
+
+});
