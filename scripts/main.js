e088d652b051df8a49a631c507dbbfa16c86d705
Aldo Mucino <amucinom@gmail.com>
2016-05-30 13:14:26 -0700

    testing removal of mobile query in js file

--- scripts/main.js
+++ scripts/main.js
@@ -11,12 +11,12 @@ $(document).ready(function() {
 
 	});
 
-	if (/Mobi/.test(navigator.userAgent)) {
-    	$('#masthead').removeClass('wrapper');
-    	$('#hero').removeClass('wrapper');
-    	$('#footer').removeClass('wrapper');
-    	$('#card-container').removeClass('wrapper');
-    	$('#subpage').removeClass('wrapper');
-	}
+	// if (/Mobi/.test(navigator.userAgent)) {
+    // 	$('#masthead').removeClass('wrapper');
+    // 	$('#hero').removeClass('wrapper');
+    // 	$('#footer').removeClass('wrapper');
+    // 	$('#card-container').removeClass('wrapper');
+    // 	$('#subpage').removeClass('wrapper');
+	// }
 
 });
